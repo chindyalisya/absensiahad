9c68ac7b730a070f87b131d10e5023169d7ef2c4
chindyalisya <chindy.alieisyia45@smk.belajar.id>
2025-03-04 10:25:32 +0700

    pembenahan title web

--- resources/views/auth/loginadmin.blade.php
+++ resources/views/auth/loginadmin.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-    <title>Sign in with illustration - Tabler</title>
+    <title>Absensi Ahad Pagi- Administrator</title>
     <!-- CSS files -->
     <link href="{{ asset('tabler/dist/css/tabler.min.css?1692870487') }}" rel="stylesheet"/>
     <link href="{{ asset('tabler/dist/css/tabler-flags.min.css?1692870487') }}" rel="stylesheet"/>
